43dac5b4108adb411f6396ebdc1e874ef37da034
Maximo <maxmonteiro86@gmail.com>
2023-10-18 14:56:52 -0300

    cria estilo da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,11 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=500" 
             />
 
-            <div className="profile">
+            <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/8462641?v=4"
+                />
                 <strong>Máximo Henrique</strong>
                 <span>Web Developer</span>
             </div>
